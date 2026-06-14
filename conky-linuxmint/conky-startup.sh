44194847d612a8e0e42a4ebea48855770648eb41
Miguel Salazar <mig.salazars@gmail.com>
2026-06-14 16:46:46 -0600

    clean - old files

--- conky-linuxmint/conky-startup.sh
+++ conky-linuxmint/conky-startup.sh
@@ -1,4 +1,9 @@
-sleep -100s
-killall conky
-cd "/home/mikengine/.conky/Conky Seamod"
-conky -c "/home/mikengine/.conky/Conky Seamod/conky_seamod" &
+#!/usr/bin/env sh
+
+sleep 10s
+killall conky 2>/dev/null
+
+script_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+cd "$script_dir/Conky Seamod" || exit 1
+
+conky -c "$script_dir/Conky Seamod/conky_seamod" &
